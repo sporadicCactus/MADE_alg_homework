03cf890bfa27da67bd70f95cc4d7ed4cd0a9ebfe
sporadicCactus <51837750+sporadicCactus@users.noreply.github.com>
2020-01-13 02:05:39 +0300

    problem 19 improved
    Переделал задачу. Теперь хэш-дерево строится по точкам, а не прямоугольникам. Получилось даже проще.
    На всякий случай ссылка на контест для новой реализации:  https://contest.yandex.ru/contest/16362/run-report/29718143/

--- pr_19.cpp
+++ pr_19.cpp
@@ -13,267 +13,168 @@ ui MAX_NUM = ~0;
 double X_BOUND = 180.;
 double Y_BOUND = 90.;
 int MAX_DEPTH = 2;
-int MAX_POINTS_IN_NODE = 16;
+int MAX_POINTS_IN_NODE = 4;
 
-struct EndPoint {
-    ui pt;
-    int box;
-    bool right;
-    EndPoint(ui pt_, int box_, bool right_): pt(pt_), box(box_), right(right_) {}
+enum RelPos {Inside, Outside, OnBorder};
+
+struct Point {
+    ui x;
+    ui y;
+    Point(ui x_, ui y_): x(x_), y(y_) {}
+    Point(double x_, double y_) {
+        x = (ui)std::round((x_ + X_BOUND)/(2*X_BOUND)*MAX_NUM);
+        y = (ui)std::round((y_ + Y_BOUND)/(2*Y_BOUND)*MAX_NUM);
+    }
 };
 
 struct HashNode {
     std::vector<HashNode*> children;
-    std::unordered_set<int> internal;
-    std::unordered_set<int> border;
+    std::vector<Point> points;
+    int n_points;
+    Point lower_left;
+    Point upper_right;
+    HashNode(Point ll, Point ur): n_points(0), lower_left(ll), upper_right(ur) {}
 };
-
-struct Box {
-    ui x_left;
-    ui x_right;
-    ui y_left;
-    ui y_right;
-    bool contains(std::pair<ui, ui> point) {
-        bool x_match = (point.first >= x_left) && (point.first < x_right);
-        bool y_match = (point.second >= y_left) && (point.second < y_right);
-        return (x_match && y_match);
-    }
-    Box(ui a1, ui a2, ui a3, ui a4): x_left(a1), x_right(a3), y_left(a2), y_right(a4) {}
-};
-
-int inline hash(ui x, int depth) {
-    return (x << (4 * depth)) >> (8*sizeof(ui) - 4);
-}
 
 class HashTree {
     private:
-    bool empty;
-    HashNode *x_root;
-    HashNode *y_root;
-    std::vector<std::vector<EndPoint> > divide_points(const std::vector<EndPoint>& points, int depth) {
-        std::vector<std::vector<EndPoint> > out(16, std::vector<EndPoint>());
-        for (int i = 0; i < points.size(); i++) {
-            out[((points[i].pt) << 4 * depth) >> (8*sizeof(ui) - 4)].push_back(points[i]);
-        }
-        return out;
-    }
-    std::vector<std::vector<EndPoint> > populate_node(HashNode* node,
-        const std::vector<EndPoint>& points, int depth) {
-        std::vector<std::vector<EndPoint> > divided;
-        if (points.size() < MAX_POINTS_IN_NODE || depth >= MAX_DEPTH) {
-            for (int i = 0; i < points.size(); i++) {
-                EndPoint point  = points[i];
-                node->internal.erase(point.box);
-                node->border.insert(point.box);
-            }
+    HashNode* root;
+
+    int hash(Point p, int depth) {
+        if (depth % 2 == 0) {
+            int x_hash = (p.x << ((depth/2)*5)) >> (8*sizeof(ui) - 3);
+            int y_hash = (p.y << ((depth/2)*5)) >> (8*sizeof(ui) - 2);
+            return x_hash + 8*y_hash;
         } else {
-            for (int i = 0; i < 16; i++) {
-                HashNode* new_node = new HashNode;
-                new_node->internal = node->internal;
-                node->children.push_back(new_node);
-            }
-            for (int i = 0; i < points.size(); i++) {
-                EndPoint point  = points[i];
-                int pos = hash(points[i].pt, depth);
-                if (point.right) {
-                    for (int j = pos + 1; j < 16; j++) {
-                        node->children[j]->internal.erase(point.box);
-                    }
-                } else {
-                    for (int j = 0; j < pos; j++ ) {
-                        node->children[j]->internal.erase(point.box);
-                    }
-                }
-                node->internal.clear();
-            }        
-            divided = divide_points(points, depth);
+            int x_hash =  (p.x << ((depth/2)*5 + 3)) >> (8*sizeof(ui) - 2);
+            int y_hash =  (p.y << ((depth/2)*5 + 2)) >> (8*sizeof(ui) - 3);
+            return x_hash + 4*y_hash;
         }
-        return divided;
     }
-    HashNode* build_hash_tree(const std::vector<EndPoint>& EndPoints) {
-        HashNode *root = new HashNode;
-        for (int i = 0; i < EndPoints.size(); i++) {
-            root->internal.insert(EndPoints[i].box);
+    void spawn_nodes(HashNode* node, int depth) {
+        ui dx = node->upper_right.x - node->lower_left.x;
+        ui dy = node->upper_right.y - node->lower_left.y;
+        int divx = depth % 2 == 0 ? 8 : 4;
+        int divy = depth % 2 == 0 ? 4 : 8;
+        for (int i = 0; i < 32; i++) {
+            Point new_lower_left = node->lower_left;
+            new_lower_left.x += (dx / divx) * (i % divx);
+            new_lower_left.y += (dy / divy) * (i / divx);
+            Point new_upper_right = new_lower_left;
+            new_upper_right.x += dx / divx;
+            new_upper_right.y += dy / divy;
+            HashNode* new_node = new HashNode(new_lower_left, new_upper_right);
+            node->children.push_back(new_node);
         }
-        std::queue<std::tuple<HashNode*, std::vector<EndPoint>, int> > build_queue;
-        build_queue.push(std::make_tuple(root, EndPoints, 0));
-        while (build_queue.size() > 0) {
-            std::tuple<HashNode*, std::vector<EndPoint>, int>& current = build_queue.front();
-            std::vector<std::vector<EndPoint> > divided = populate_node(std::get<0>(current),
-                std::get<1>(current), std::get<2>(current));
-            if (divided.size() == 0) {
-                build_queue.pop();
-                continue;
-            }
-            for (int i = 0; i < 16; i++) {
-                build_queue.push(std::make_tuple(std::get<0>(current)->children[i],
-                    divided[i], std::get<2>(current) + 1));
-            }
-            build_queue.pop();
+        for (int i = 0; i < node->points.size(); i++) {
+            Point pt = node->points[i];
+            int pt_hash = hash(pt, depth);
+            node->children[pt_hash]->points.push_back(pt);
+            node->children[pt_hash]->n_points++;
         }
-        return root;
+        node->points.clear();
     }
-    void delete_subtree(HashNode *root) {
-        
-        std::queue<HashNode*> node_queue;
-        node_queue.push(root);
-        while(node_queue.size() > 0) {
-            HashNode* current = node_queue.front();
-            node_queue.pop();
+    RelPos compare_node_to_box(HashNode* node, Point lower_left, Point upper_right) {
+        if ((node->lower_left.x >= upper_right.x) || (node->lower_left.y >= upper_right.y)) return Outside;
+        if ((node->upper_right.x <= lower_left.x) || (node->upper_right.y <= lower_left.y)) return Outside;
+        if ((node->lower_left.x >= lower_left.x) && (node->lower_left.y >= lower_left.y) &&
+            (node->upper_right.x <= upper_right.x) && (node->upper_right.y <= upper_right.y)) return Inside;
+        return OnBorder;
+    }
+    bool point_in_box(Point p, Point lower_left, Point upper_right) {
+        bool res = true;
+        res = res && (p.x >= lower_left.x);
+        res = res && (p.y >= lower_left.y);
+        res = res && (p.x < upper_right.x);
+        res = res && (p.y < upper_right.y);
+        return res;
+    }
+
+
+    public:
+    HashTree() {
+        root = new HashNode(Point((ui)0,(ui)0), Point(MAX_NUM, MAX_NUM));
+    }
+    ~HashTree() {
+        std::queue<HashNode*> queue;
+        queue.push(root);
+        while (queue.size() > 0) {
+            HashNode* current = queue.front();
+            queue.pop();
             for (int i = 0; i < current->children.size(); i++) {
-                node_queue.push(current->children[i]);
+                queue.push(current->children[i]);
             }
             delete current;
         }
     }
-
-    public:
-    HashTree(): empty(true) {}
-    ~HashTree() {
-        if (!empty) {
-            delete_subtree(x_root);
-            delete_subtree(y_root);
-        }
-    }
-    void Build(const std::vector<EndPoint>& endpoints_x, const std::vector<EndPoint>& endpoints_y) {
-        if (!empty) return;
-        x_root = build_hash_tree(endpoints_x);
-        y_root = build_hash_tree(endpoints_y);
-        empty = false;
-    }
-    std::pair<std::unordered_set<int>, std::unordered_set<int>>
-        ProcessPoint(std::pair<ui, ui> point) {
-        HashNode *current_x = x_root;
+    HashTree(const HashTree& other) = delete;
+    HashTree& operator=(const HashTree& other) = delete;
+    HashTree(HashTree&& other) = delete;
+    HashTree& operator=(HashTree&&) = delete;
+    void AddPoint(Point pt) {
         int depth = 0;
-        while (current_x->children.size() > 0) {
-            current_x = current_x->children[hash(point.first, depth)];
+        HashNode* current_node = root;
+        while (current_node->children.size() != 0) {
+            current_node->n_points++;
+            current_node = current_node->children[hash(pt, depth)];
             depth++;
         }
-        HashNode *current_y = y_root;
-        depth = 0;
-        while (current_y->children.size() > 0) {
-            current_y = current_y->children[hash(point.second, depth)];
-            depth++;
-        }
-        std::unordered_set<int> certain;
-        std::unordered_set<int> uncertain;
-        std::unordered_set<int> *short_internal = &current_x->internal; 
-        std::unordered_set<int> *long_internal = &current_y->internal;
-        std::unordered_set<int> *short_border = &current_x->border; 
-        std::unordered_set<int> *long_border = &current_y->border;
-        if (short_internal->size() > long_internal->size()) {
-            short_internal = &current_y->internal; 
-            long_internal = &current_x->internal;
-            short_border = &current_y->border; 
-            long_border = &current_x->border;   
-        } 
-        for (int box_num : *short_internal) {
-            if (long_internal->find(box_num) != long_internal->end()) {
-                certain.insert(box_num);
-            }
-            if (long_border->find(box_num) != long_border->end()) {
-                uncertain.insert(box_num);
-            }
-        }
-        for (int box_num : *short_border) {
-            if (long_internal->find(box_num) != long_internal->end()) {
-                uncertain.insert(box_num);
-            }
-            if (long_border->find(box_num) != long_border->end()) {
-                uncertain.insert(box_num);
-            }
-        }
-        return std::make_pair(certain, uncertain);
-    }
-};
-
-class Counter {
-    private:
-    std::vector<Box> boxes;
-    HashTree hashtree;
-    bool tree_built;
-    std::vector<int> counter;
-
-    public:
-    Counter(): tree_built(false) {}
-    void AddBox(Box new_box) {
-        if (tree_built) return;
-        boxes.push_back(new_box);
-    }
-    void Build() {
-        std::vector<EndPoint> endpoints_x;
-        std::vector<EndPoint> endpoints_y;
-        for (int i = 0; i < boxes.size(); i++) {
-            Box box = boxes[i];
-            endpoints_x.push_back(EndPoint(box.x_left, i, false));
-            endpoints_x.push_back(EndPoint(box.x_right, i, true));
-            endpoints_y.push_back(EndPoint(box.y_left, i, false));
-            endpoints_y.push_back(EndPoint(box.y_right, i, true));
-        }
-        hashtree.Build(endpoints_x, endpoints_y);
-        counter = std::vector<int>(boxes.size(), 0);
-        tree_built = true;
-    }
-    void AddPoint(std::pair<ui, ui> point) {
-        if (!tree_built) return;
-        std::pair<std::unordered_set<int>, std::unordered_set<int>>
-            tree_output = hashtree.ProcessPoint(point);
-        for (int box_num :  tree_output.first) {
-            counter[box_num]++;
-        }
-        for (int box_num : tree_output.second) {
-            if (boxes[box_num].contains(point)) counter[box_num]++;
+        current_node->n_points++;
+        current_node->points.push_back(pt);
+        if ((current_node->points.size() > MAX_POINTS_IN_NODE) && (depth < MAX_DEPTH)) {
+            spawn_nodes(current_node, depth);
         }
     }
-    const std::vector<int>& GetCounts() const {
+    int CountPointsInBox(Point lower_left, Point upper_right) {
+        int counter = 0;
+        std::queue<HashNode*> queue;
+        queue.push(root);
+        while (queue.size() > 0) {
+            HashNode* current = queue.front();
+            queue.pop();
+            RelPos rel_pos = compare_node_to_box(current, lower_left, upper_right);
+            if (rel_pos == Inside) {
+                counter += current->n_points;
+                continue;
+            }
+            if (rel_pos == Outside) continue;
+            for (int i = 0; i < current->children.size(); i++) {
+                queue.push(current->children[i]);
+            }
+            if (current->children.size() > 0) continue;
+            for (int i = 0; i < current->points.size(); i++) {
+                if (point_in_box(current->points[i], lower_left, upper_right)) counter++;
+            }
+        }
         return counter;
-    } 
+    }
 };
-
-ui round_x(double x) {
-    return (ui)std::round((x + X_BOUND)/(2*X_BOUND)*MAX_NUM);
-}
-
-ui round_y(double y) {
-    return (ui)std::round((y + Y_BOUND)/(2*Y_BOUND)*MAX_NUM);
-}
 
 int main() {
+    HashTree hash_tree;
     std::ifstream input("input.txt");
     std::ofstream output("output.txt");
     int n_points;
     input >> n_points;
-    std::vector<std::pair<ui, ui> > points;
     for (int i = 0; i < n_points; i++) {
         double x_coord;
         input >> x_coord;
         double y_coord;
         input >> y_coord;
-        ui int_x = round_x(x_coord);
-        ui int_y = round_y(y_coord);
-        points.push_back(std::make_pair(int_x, int_y));
+        hash_tree.AddPoint(Point(x_coord, y_coord));
     }
-    Counter counter;
     int n_boxes;
     input >> n_boxes;
     for (int i = 0; i < n_boxes; i++) {
-        double x_left;
-        input >> x_left;
-        double y_left;
-        input >> y_left;
-        double x_right;
-        input >> x_right;
-        double y_right;
-        input >> y_right;
-        counter.AddBox(Box(round_x(x_left), round_y(y_left), round_x(x_right), round_y(y_right)));
-    }
-    counter.Build();
-    for (int i = 0; i < n_points; i++) {
-        counter.AddPoint(points[i]);
-    }
-    const std::vector<int>& counts = counter.GetCounts();
-    for (int i = 0; i < n_boxes; i++) {
-        output << counts[i] << '\n';
+        double x_ll;
+        input >> x_ll;
+        double y_ll;
+        input >> y_ll;
+        double x_ur;
+        input >> x_ur;
+        double y_ur;
+        input >> y_ur;
+        std::cout << hash_tree.CountPointsInBox(Point(x_ll, y_ll), Point(x_ur, y_ur)) << '\n';
     }
     return 0;
 }
